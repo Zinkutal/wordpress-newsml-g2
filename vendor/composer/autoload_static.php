2447e2c2e7ec0b2023d39698032575710181da1d
alexandrkucherov <zinkutal@gmail.com>
2021-02-26 16:47:10 +0700

    added phpcs rules && gh actions support

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,10 +4,9 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInited4cdd33916205b509353c312962cdec
+class ComposerStaticInit20f99ef0485ddf8e755b5ec51da56dcf
 {
     public static $files = array (
-        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     );
 
@@ -402,9 +401,9 @@ class ComposerStaticInited4cdd33916205b509353c312962cdec
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInited4cdd33916205b509353c312962cdec::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInited4cdd33916205b509353c312962cdec::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInited4cdd33916205b509353c312962cdec::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit20f99ef0485ddf8e755b5ec51da56dcf::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit20f99ef0485ddf8e755b5ec51da56dcf::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit20f99ef0485ddf8e755b5ec51da56dcf::$classMap;
 
         }, null, ClassLoader::class);
     }
